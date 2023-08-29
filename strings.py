404627f57c54c69d74d2d9e8353cbe0d9f3103e0
James L Hoholik <jhoholik32@gmail.com>
2023-08-29 14:43:16 -0700

    added breaks in string of code using \ and \n

---
+++ strings.py
@@ -0,0 +1,6 @@
+street = "Summit_Ave"
+print(street)
+lyrics = "ladies and gentlmen good evening please check to see if your still \
+    \nbreathing the show is almost over please come in a little\
+    \n bit closer welcome to the show please come inside ladies and gentlemen"
+print(lyrics)
