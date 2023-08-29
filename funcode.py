a26e6addb26966eda7b6228ab1747d4b5d438571
James L Hoholik <jhoholik32@gmail.com>
2023-08-29 13:51:07 -0700

    added intergers and floats as well on how to get both

--- funcode.py
+++ funcode.py
@@ -1,7 +1,18 @@
 # this is my first print statement
-print('hello world')
-print('my name is james')
-print('this is my first python lesson')
-# declared a varible "firstname" assigned it the value "James"
-firstname = 'James'
-print(firstname)
+# print('hello world')
+# print('my name is james')
+# print('this is my first python lesson')
+# declared a varible "firstname" assigned it the value "
+# firstname = 'james'
+# # lastname = 'hoholik'
+# print(firstname)
+# print(lastname)
+# print(1.1+3.1)
+# print(1.5*2)
+# print(1-3.765)
+my_int = 2
+my_float = float(my_int)
+print(my_float)
+my_float2 = 2.77
+my_int_2 =int(my_float2)
+print(my_int_2)
